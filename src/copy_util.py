9c49b0eca22735995e48b0a792e33d1ec35e5fe3
Long Wei <yilang2007lw@gmail.com>
2012-06-01 19:45:58 +0800

    copy directory ok,need fix progress bug

--- src/copy_util.py
+++ src/copy_util.py
@@ -20,143 +20,141 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import gtk
 import gio
+import os
+import gtk
 import glib
 
-NONE_CALLBACK=lambda *args:None
-
 class CopyUtil():
-    '''copy files,most stolen from fmd/fsutils'''
+    
     def __init__(self):
-        self.job_queue=[]
-        self.is_run=False
-        self.cancel=gio.Cancellable()
-        self.filelist=[]
-        self.target=None
         self.source=None
+        self.target=None
+        self.progressbar=None
+        self.copy_callback_data=""
+        self.progress_callback_data=""
+        self.total_bytes=0
+        self.current_bytes=0
+        self.end_bytes=0
+        self.filelist=[]
 
-    def get_target_gfile(self,target_path):
-        self.target=gio.File(target_path)
-        target_type=self.target.query_info("standard::type",gio.FILE_QUERY_INFO_NOFOLLOW_SYMLINKS).get_file_type()
-        if target_type==gio.FILE_TYPE_DIRECTORY:
-            return self.target
-        else:
-            print "need create a directory has the same path"
-
-    def get_source_filelist(self,source_path):
+    def get_source_gfilelist(self,source_path):
+        '''get source gfilelist'''
         self.source=gio.File(source_path)
         source_type=self.source.query_info("standard::type",gio.FILE_QUERY_INFO_NOFOLLOW_SYMLINKS).get_file_type()
         if source_type==gio.FILE_TYPE_DIRECTORY:
-            print "need insert all the path gfile object to filelist"
-        else:
+            for dirpath,dirnames,filenames in os.walk(source_path):
+                for dirname in dirnames:
+                    fullpath=os.path.join(dirpath,dirname)
+                    self.filelist.append(gio.File(fullpath))
+                    
+                for filename in filenames:
+                    fullpath=os.path.join(dirpath,filename)
+                    self.filelist.append(gio.File(fullpath))
+
+        else:            
             self.filelist.append(self.source)
+        return self.filelist    
 
-    def job_move(self,source,target):
-        source.copy_async(target,self.job_done,NONE_CALLBACK,gio.FILE_COPY_NOFOLLOW_SYMLINKS,
-                          cancellable=self.cancel,user_data=True)
+    def get_total_num_bytes(self,filelist):
+        '''return the size of source_path'''
+        for f in filelist:
+            fi=f.query_info(gio.FILE_ATTRIBUTE_STANDARD_SIZE)
+            fsize=fi.get_attribute_uint64(gio.FILE_ATTRIBUTE_STANDARD_SIZE)
+            self.total_bytes+=fsize
 
-    def job_move_dir(self,source,target):
-        pass
-
-    def job_copy(self,source,target):
-        source.copy_async(target,self.job_done,NONE_CALLBACK,gio.FILE_COPY_NOFOLLOW_SYMLINKS,
-                          cancellable=self.cancel,user_data=False)
-    def idle(self):
-        try:
-            job=self.job_queue.pop(0)
-        except IndexError:
-            self.is_run=False
-            return False
-
-        job[0](*job[1:])
-        return False
-
-    def check_and_run(self):
-        if not self.is_run:
-            self.is_run=True
-            glib.idle_add(self.idle)
+        return self.total_bytes
     
-    def copy(self,filelist,target):
-        for f in filelist:
-            fi=f.query_info("standard::type",gio.FILE_QUERY_INFO_NOFOLLOW_SYMLINKS)
-            tf=target.get_child(f.get_basename())
-            self.push_copy_task(f,tf,fi.get_file_type())
+    def get_target_gfile(self,srcfile,target):
+        '''get target gfile object'''
+        relative_path=self.source.get_relative_path(srcfile)
+        target_gfile=target.resolve_relative_path(relative_path)
 
-        self.check_and_run()
+        return target_gfile    
 
-    def push_copy_task(self,source,target,source_type=None):
-        if not source_type:
-            source_type=source.query_info("standard::type",gio.FILE_QUERY_INFO_NOFOLLOW_SYMLINKS).get_file_type()
-        if source_type==gio.FILE_TYPE_DIRECTORY:
-            self.job_queue.append(None,source,target)
+    def copy_list(self,filelist,target_path):
+        '''copy all gfile object to the given target_path'''
+        target=gio.File(target_path)
+        target_type=target.query_info("standard::type",gio.FILE_QUERY_INFO_NOFOLLOW_SYMLINKS).get_file_type()
+
+        if target_type==gio.FILE_TYPE_DIRECTORY:
+            for f in filelist:
+                target_gfile=self.get_target_gfile(f,target)
+
+                if f.query_info("standard::type",gio.FILE_QUERY_INFO_NOFOLLOW_SYMLINKS).get_file_type()==gio.FILE_TYPE_DIRECTORY:
+                    if target_gfile.query_exists(None):
+                        continue
+                    if not target_gfile.get_parent():
+                        target_gfile.make_directory_with_parents()
+                    else:
+                        target_gfile.make_directory()
+                else:        
+                    if target_gfile.query_exists():
+                        target_gfile.delete()
+                    self.start_copy(f,target_gfile)
+
         else:
-            self.job_queue.append(self.job_copy,source,target)
+            print "must supply a directory to copy data"
 
-    def push_move_task(self,source,target,source_type=None):
-        if not source_type:
-            source_type=source.query_info("standard::type",gio.FILE_QUERY_INFO_NOFOLLOW_SYMLINKS).get_file_type()
-        if source_type==gio.FILE_TYPE_DIRECTORY:
-            self.job_queue.append(self.job_move_dir,source,target)
-        else:
-            self.job_queue.append(self.job_move,source,target)
+    def start_copy(self,srcfile,dstfile):
+        srcfile.copy_async(dstfile,self.finish_copy_callback,self.progress_copy_callback,
+                           flags=gio.FILE_COPY_OVERWRITE,io_priority=glib.PRIORITY_DEFAULT,cancellable=None,
+                           user_data=self.copy_callback_data,
+                           progress_callback_data=self.progress_callback_data)
+        # print "start copy "+srcfile.get_path()+" to "+dstfile.get_path()
+
+
+    def finish_copy_callback(self,srcfile,result,copy_callback_data):
+        '''finish copy a single file or directory'''
+        print "finish copy:"+srcfile.get_path()
+        srcfile.copy_finish(result)
+        # self.progressbar.set_text("Finished Copy")
+        # self.progressbar.set_fraction(1.0)
+
+    def progress_copy_callback(self,current_num_bytes,end_num_bytes,progress_callback_data):
+        # self.global_current_bytes=self.end_bytes+current_num_bytes
+        # self.end_bytes+=end_num_bytes
+        # self.progressbar.set_text(str(self.global_current_bytes/1024)+"K/"+str(self.total_bytes)+"K:"
+        #                           +str(self.global_current_bytes*100/self.total_bytes*1024)+"%")
+        # self.progressbar.set_fraction(self.global_current_bytes/self.total_bytes*1024)
+        self.progressbar.set_text(str(current_num_bytes)+" / "+str(end_num_bytes))
+        self.progressbar.set_fraction(current_num_bytes/end_num_bytes)
+        print "current_num_bytes:"
+        print current_num_bytes
+        print "end_num_bytes:"
+        print end_num_bytes
+
+    def test_copy_single(self):
+        self.window=gtk.Window(gtk.WINDOW_TOPLEVEL)
+        self.window.set_size_request(400,300)
+        self.window.set_title("Copy Util")
+        self.window.set_position(gtk.WIN_POS_CENTER)
+        self.vbox=gtk.VBox(False,1)
+        self.label=gtk.Label("Copy Util")
+
+        self.progressbar=gtk.ProgressBar()
+        self.progressbar.set_text("Copying......")
         
-    def move(self,filelist,target):
-        target_fs=target.query_info("id::filesystem").get_attribute_as_string("id::filesystem")
-        for f in filelist:
-            if target.equal(f.get_parent()):
-                continue
-            fi=f.query_info("standard::type,id::filesystem",gio.FILE_QUERY_INFO_NOFOLLOW_SYMLINKS)
-            source_fs=fi.get_attribute_as_string("id::filesystem")
+        self.vbox.pack_start(self.label)
+        self.vbox.pack_start(self.progressbar)
+        self.window.add(self.vbox)
+        self.window.connect("destroy",gtk.main_quit)
+        self.window.show_all()
 
-            tf=target.get_child(f.get_basename())
-            if source_fs==target_fs:
-                f.move(tf,NONE_CALLBACK,gio.FILE_QUERY_INFO_NOFOLLOW_SYMLINKS)
-            else:
-                self.push_move_task(f,tf,fi.get_file_type())
-                
-        self.check_and_run()        
 
-    def cancel(self):
-        pass
+        srcfile=gio.File("deepin.iso")
+        dstfile=gio.File("deepin_copy.iso")
 
-    #     self.src=gio.File(src)
-    #     self.dst=gio.File(dst)
-    #     info=self.src.query_info(gio.FILE_ATTRIBUTE_STANDARD_SIZE)
-    #     self.total_num_bytes=info.get_attribute_uint64(gio.FILE_ATTRIBUTE_STANDARD_SIZE)
-    #     print self.total_num_bytes
-
-    #     self.window=gtk.Window(gtk.WINDOW_TOPLEVEL)
-    #     self.window.set_size_request(400,300)
-    #     self.window.set_title("Copy Util")
-    #     self.window.set_position(gtk.WIN_POS_CENTER)
-    #     self.vbox=gtk.VBox(False,1)
-    #     self.progressbar=gtk.ProgressBar()
-    #     self.progressbar.set_text("Copying......")
+        self.start_copy(srcfile,dstfile)
         
-    #     self.vbox.pack_start(self.progressbar)
-    #     self.window.add(self.vbox)
-    #     self.window.show_all()
-    #     self.window.connect("destroy",gtk.main_quit)
-
-    # def start_copy(self):
-    #     self.src.copy_async(self.dst,self.finish_copy_callback,self.progress_copy_callback,gio.FILE_COPY_OVERWRITE,glib.PRIORITY_DEFAULT,None,self.progressbar,self.progressbar)
-    #     gtk.main()
-
-    # def finish_copy_callback(self,srcfile,result,progressbar):
-    #     self.src.copy_finish(result)
-    #     self.progressbar.set_text("Finished Copy")
-    #     self.progressbar.set_fraction(1.0)
-    #     print "finish copy"
-
-    # def progress_copy_callback(self,current_num_bytes,total_num_bytes,progressbar):
-    #     total_num_bytes=self.total_num_bytes
-
-    #     self.progressbar.set_text(str(current_num_bytes/1024)+"K/"+str(total_num_bytes/1024)+"K")
-    #     self.progressbar.set_fraction(current_num_bytes/total_num_bytes)
-
 if __name__=="__main__":
-   # cu=CopyUtil("testsrc","testtarget")
-   # # cu=CopyUtil("/media/cdrom/casper/filesystem.squashfs","/target")
-   # cu.start_copy()
-    pass
+    cu=CopyUtil()
+    # filelist=cu.get_source_gfilelist("/home/yilang/Project/deepin-installer")
+    # cu.total_bytes=cu.get_total_num_bytes(filelist)
+
+    # cu.copy_list(filelist,"/home/yilang/test")
+
+    cu.test_copy_single()
+    gtk.main()
+    # cu.get_source_gfilelist("/home/yilang/Project/deepin-installer")
+    
