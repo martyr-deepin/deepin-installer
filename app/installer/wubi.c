3a98982ed2ff5f1bcd00e488244de94bee3e93a8
yilang <yilang2007lw@gmail.com>
2014-04-22 16:18:34 +0800

    update wubi

--- app/installer/wubi.c
+++ app/installer/wubi.c
@@ -21,7 +21,7 @@
 
 #include "wubi.h"
 
-#define WUBI_CONFIG     /target/preseed.cfg
+#define WUBI_CONFIG_FILE     /target/preseed.cfg
 
 JS_EXPORT_API 
 gboolean installer_is_use_wubi ()
@@ -41,6 +41,77 @@ gboolean installer_is_use_wubi ()
 JS_EXPORT_API 
 JSObjectRef installer_get_wubi_config ()
 {
+    GRAB_CTX ();
     JSObjectRef json = json_create ();
+
+    gsize index = 0;
+    GError *error = NULL;
+    GFile *file = NULL;
+    GFileInputStream *input = NULL;
+    GDataInputStream *data_input = NULL;
+
+    file = g_file_new_for_path (WUBI_CONFIG_FILE);
+    if (!g_file_query_exists (file, NULL)) {
+        g_warning ("get wubi config:%s not exists\n", WUBI_CONFIG_FILE);
+        goto out;
+    }
+
+    input = g_file_read (file, NULL, &error);
+    if (error != NULL){
+        g_warning ("get wubi config:read config file error->%s", error->message);
+        goto out;
+    }
+
+    data_input = g_data_input_stream_new ((GInputStream *) input);
+    if (data_input == NULL) {
+        g_warning ("get wubi config:get data input stream failed\n");
+        goto out;
+    }
+    
+    char *data = (char *) 1;
+    while (data) {
+        gsize length = 0;
+        data = g_data_input_stream_read_line (data_input, &length, NULL, &error);
+        if (error != NULL) {
+            g_warning ("get wubi config:read line error->%s", error->message);
+            g_error_free (error);
+            error = NULL;
+            continue;
+        }
+        if (data != NULL) {
+            if (g_str_has_prefix (data, "d-i")){
+                gchar **line = g_strsplit (data, "\t", -1);
+                if (line == NULL) {
+                    g_warning ("get wubi config:split %s failed\n", data);
+                } else {
+                    if (g_strcmp0 (line[1], "netcfg/get_hostname") == 0) {
+                        json_append_string (json, "hostname", line[3]);
+                    } else if (g_strcmp0 (line[1], "passwd/username") == 0) {
+                        json_append_string (json, "username", line[3]);
+                    } else if (g_strcmp0 (line[1], "passwd/user-passwd-crypted") == 0) {
+                        json_append_string (json, "user-passwd-crypted", line[3]);
+                    } else if (g_strcmp0 (line[1]. "time/zone") == 0) {
+                        json_append_string (json, "timezone", line[3]);
+                    }
+                    g_strfreev (line);
+                }
+            }
+        } 
+    }
+    goto out;
+out:
+    if (file != NULL) {
+        g_object_unref (file);
+    }
+    if (data_input != NULL) {
+        g_object_unref (data_input);
+    }
+    if (input != NULL) {
+        g_object_unref (input);
+    }
+    if (error != NULL) {
+        g_error_free (error);
+    }
+    UNGRAB_CTX ();
     return json;
 }
