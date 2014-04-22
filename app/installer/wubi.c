3705c1056c49970096f463f2b37e4d6d2b806f38
yilang <yilang2007lw@gmail.com>
2014-04-22 15:44:32 +0800

    add wubi

---
+++ app/installer/wubi.c
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2011 ~ 2013 Deepin, Inc.
+ *               2011 ~ 2013 Long Wei
+ *
+ * Author:      Long Wei <yilang2007lw@gmail.com>
+ * Maintainer:  Long Wei <yilang2007lw@gamil.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ **/
+
+#include "wubi.h"
+
+#define WUBI_CONFIG     /target/preseed.cfg
+
+JS_EXPORT_API 
+gboolean installer_is_use_wubi ()
+{
+    gint status = -1;
+    g_spawn_command_line_sync ("grep install-automatic /proc/cmdline", NULL, NULL, &status, NULL); 
+    if (status == 0) {
+        return TRUE;
+    }
+    g_spawn_command_line_sync ("grep install-nodesktop /proc/cmdline", NULL, NULL, &status, NULL); 
+    if (status == 0) {
+        return TRUE;
+    }
+    return FALSE;
+}
+
+JS_EXPORT_API 
+JSObjectRef installer_get_wubi_config ()
+{
+    JSObjectRef json = json_create ();
+    return json;
+}
