726f1c8d0bd50e3eb5f6a52893baf5dd308f330b
bluth653426595 <ycl_pj@163.com>
2014-05-26 17:35:04 +0800

    add post-install/test.sh

---
+++ resources/post-install/test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo testdeepin-installer
+echo cd /tmp/
+cd /tmp/
+echo touch testdeepin-install.txt
+touch testdeepin-install.txt
+echo sudo cp testdeepin-install.txt /usr/share/deepin-installer/resources/
+sudo cp testdeepin-install.txt /usr/share/deepin-installer/resources/
+
