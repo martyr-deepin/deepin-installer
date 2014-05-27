c9c67c17ef33288905b6f7f452ac9b6184986857
bluth653426595 <ycl_pj@163.com>
2014-05-27 09:22:22 +0800

    update test.sh

--- resources/post-install/test.sh
+++ resources/post-install/test.sh
@@ -5,6 +5,6 @@ echo cd /tmp/
 cd /tmp/
 echo touch testdeepin-install.txt
 touch testdeepin-install.txt
-echo sudo cp testdeepin-install.txt /usr/share/deepin-installer/resources/
-sudo cp testdeepin-install.txt /usr/share/deepin-installer/resources/
+echo sudo cp testdeepin-install.txt /usr/share/installer/resources/
+sudo cp testdeepin-install.txt /usr/share/installer/resources/
 
