4c5a6b0851c3aed4d8844562719ea65b98132b28
leaeasy <leaeasy@gmail.com>
2014-06-12 15:16:17 +0800

    fix end.sh.

--- resources/hooks/70remove-overlaymirror.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-# Remove overlay-mirror in squashfs
-if [ -d /opt/overlay-mirror ]; then
-    rm -rf /opt/overlay-mirror
-fi
-
-exit 0
--- resources/hooks/end.sh
+++ resources/hooks/end.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
-hookspath=/usr/share/installer/resources/hooks
+HOOKSPATH=/usr/share/installer/resources/hooks
 
-echo test deepin-installer
-
-echo cd $hookspath/
-cd $hookspath/
-
-echo touch /tmp/test-deepin-install-ok.txt
-touch /tmp/test-deepin-install-ok.txt
-
-echo ./55-netcfg-network-manager
-./55-netcfg-network-manager
-
-echo ./56enable-network-manager
-./56enable-network-manager
-
-echo ./70remove-overlaymirror.sh
-./70remove-overlaymirror.sh
+if [ -f ${HOOKSPATH}/ORDER ];then
+    . ${HOOKS}/ORDER
+fi
 
 exit 0
