b6ec85272b40de341558df93e2ccb3947ff3e349
snyh <snyh@snyh.org>
2014-06-20 13:00:08 +0800

    write bootloader info to /target/host/config.json

--- app/installer/info.c
+++ app/installer/info.c
@@ -8,5 +8,15 @@ void write_bootloader_info(const char* uuid, gboolean uefi)
 	    "}\n", 
 	    uefi ? "uefi" : "bios",
 	    uuid);
-    printf("%s\n", content);
+
+    extern const gchar *target;
+    char* path= g_strdup_printf("%s/host/config.json", target);
+    GError* error = NULL;
+    g_file_set_contents(path, content, -1, &error);
+    g_free(path);
+
+    if (error != NULL) {
+	g_warning("Write_bootloader_info failed:%s\n", error->message);
+	g_error_free(error);
+    }
 }
