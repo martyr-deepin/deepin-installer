ae871fb669059a3ba3056b2193f0fa9c883c8c19
snyh <snyh@snyh.org>
2014-06-20 15:00:00 +0800

    change json format to conf

--- app/installer/info.c
+++ app/installer/info.c
@@ -1,20 +1,18 @@
 #include "info.h"
 #include <glib.h>
-void write_bootloader_info(const char* uuid, gboolean uefi)
+void write_bootloader_info(const char* target, gboolean uefi)
 {
-    char* content = g_strdup_printf("{\"bootloader\": {\n"
-	    "\t\"type\": \"%s\"\n"
-	    "\t\"uuid\": \"%s\"\n"
-	    "\t}\n"
-	    "}\n", 
-	    uefi ? "uefi" : "bios",
-	    uuid);
+    char* content = g_strdup_printf("\n"
+	    "GRUB_TARGET=\"%s\"\n",
+	    target);
 
     GError* error = NULL;
-    g_file_set_contents("/etc/deepin-installer.json", content, -1, &error);
+    g_file_set_contents("/etc/deepin-installer.conf", content, -1, &error);
 
     if (error != NULL) {
 	g_warning("Write_bootloader_info failed:%s\n", error->message);
 	g_error_free(error);
     }
 }
+
+
