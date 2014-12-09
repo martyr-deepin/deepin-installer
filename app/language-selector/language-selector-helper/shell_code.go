5d74cf5337282d81a3dcac9178300f30c846b21b
leaeasy <leaeasy@gmail.com>
2014-12-09 18:01:43 +0800

    change localtime by locale variable
    Change-Id: I9ee76b9462d12d873b3c10074e3020cea28d51d7
    Tower: https://tower.im/projects/8f19f0bf0e754f0b82ef2c24bc230973/todos/ad2ef89cff0e43289d33f7c128bbca2c/#a38631ef3d974602a41b87e87539db43

--- app/language-selector/language-selector-helper/shell_code.go
+++ app/language-selector/language-selector-helper/shell_code.go
@@ -43,5 +43,14 @@ LANG=$(grep "^${locale}" /usr/share/i18n/SUPPORTED | grep UTF-8 | sed -e 's/ .*/
 echo $LANG
 printf 'LANGUAGE="%s"\nLANG="%s"' "${LANG%%.*}" "${LANG}" > /etc/default/locale
 printf '%s UTF-8' "${LANG}" > /etc/locale.gen
+case $LANG in 
+    zh_CN*)
+        ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+        ;;
+    zh_TW*)
+        ln -sf /usr/share/zoneinfo/Asia/Taipei /etc/localtime
+        ;;
+    *)
+esac
 /usr/sbin/locale-gen ${LANG}
 `
