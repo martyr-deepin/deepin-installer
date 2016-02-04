a05a8c1b8eb9bc69c6dbf3e1fc5edad81e25051f
Xu Shaohua <xushaohua@linuxdeepin.com>
2016-02-04 14:25:10 +0800

    improve-copyright
    Change-Id: Ief73ca55229cf1815de2eb5eed8e770e9aea4ae7

--- app/installer/installer.c
+++ app/installer/installer.c
@@ -1,22 +1,10 @@
 /**
- * Copyright (c) 2011 ~ 2013 Deepin, Inc.
- *               2011 ~ 2013 Long Wei
- *
- * Author:      Long Wei <yilang2007lw@gmail.com>
- * Maintainer:  Long Wei <yilang2007lw@gamil.com>
+ * Copyright (C) 2015 Deepin Technology Co., Ltd.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, see <http://www.gnu.org/licenses/>.
  **/
 
 #include <gtk/gtk.h>
--- app/language-selector/theme.c
+++ app/language-selector/theme.c
@@ -1,25 +1,12 @@
 /**
- * Copyright (c) 2011 ~ 2013 Deepin, Inc.
- *               2011 ~ 2012 snyh
- *               2013 ~ 2013 Liqiang Lee
- *
- * Author:      snyh <snyh@snyh.org>
- * Maintainer:  snyh <snyh@snyh.org>
- *              Liqiang Lee <liliqiang@linuxdeepin.com>
+ * Copyright (C) 2015 Deepin Technology Co., Ltd.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, see <http://www.gnu.org/licenses/>.
  **/
+
 #include <gtk/gtk.h>
 #include "background.h"
 #include "theme.h"
